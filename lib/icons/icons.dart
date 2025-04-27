9db78ad6761b550e7412bfbde89c4dc2896221c2
Newton Maluvana <andzanimaluvana@gmail.com>
2025-04-27 14:04:33 +0200

    adding icons to the project

---
+++ lib/icons/icons.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
+
+class icons {
+  static double size = 50;
+  static Color color1 = Colors.blue;
+
+  ///navigation icons
+  static Icon message = Icon(
+    HugeIcons.strokeRoundedMessage01,
+    size: size,
+    color: color1,
+  );
+  static Icon likes = Icon(
+    HugeIcons.strokeRoundedFavourite,
+    size: size,
+    color: color1,
+  );
+  static Icon account = Icon(
+    HugeIcons.strokeRoundedUserCircle,
+    size: size,
+    color: color1,
+  );
+  static Icon exlpore = Icon(
+    HugeIcons.strokeRoundedGlobalSearch,
+    size: size,
+    color: color1,
+  );
+
+  //aditional icons***************************************************
+  //symbol for male representetion
+  static Icon male = Icon(
+    HugeIcons.strokeRoundedMaleSymbol,
+    size: size,
+    color: color1,
+  );
+  //symbol for female representetion
+  static Icon female = Icon(
+    HugeIcons.strokeRoundedFemaleSymbol,
+    size: size,
+    color: color1,
+  );
+
+  //right facing arrow
+  static Icon arrow = Icon(
+    HugeIcons.strokeRoundedArrowLeft02,
+    size: size,
+    color: color1,
+  );
+
+  //facebook icon
+  static Icon facebook = Icon(
+    HugeIcons.strokeRoundedFacebook01,
+    size: size,
+    color: color1,
+  );
+  //smiking  symbol
+  static Icon smoke = Icon(Icons.smoke_free_rounded, size: size, color: color1);
+  //home house  symbol
+  static Icon house = Icon(
+    HugeIcons.strokeRoundedHome11,
+    size: size,
+    color: color1,
+  );
+  //location  symbol
+  static Icon location = Icon(
+    HugeIcons.strokeRoundedLocation01,
+    size: size,
+    color: color1,
+  );
+  static Icon drink = Icon(
+    HugeIcons.strokeRoundedSoftDrink01,
+    size: size,
+    color: color1,
+  );
+}
