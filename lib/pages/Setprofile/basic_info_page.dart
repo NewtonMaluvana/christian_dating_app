24d5e3fdf79239fbeea957399a0c77f31deff604
Newton Maluvana <andzanimaluvana@gmail.com>
2025-05-16 13:51:24 +0200

    creating BsiscInfo page

---
+++ lib/pages/Setprofile/basic_info_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BasicInfoPage extends StatefulWidget {
+  const BasicInfoPage({super.key});
+
+  @override
+  State<BasicInfoPage> createState() => _BasicInfoPageState();
+}
+
+class _BasicInfoPageState extends State<BasicInfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
